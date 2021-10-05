215a0e384455ff7013e9ad05af1c5adbd878c4d6
BulletTide <cameronsmith296@gmail.com>
2021-10-05 19:10:35 +0200

    added typings file and updated to djs v13.2

--- basic/handler/command.ts
+++ basic/handler/command.ts
@@ -6,8 +6,9 @@
           the main repo.
 */
 
-import { PermissionString, ApplicationCommandPermissionData, CommandInteraction, ApplicationCommandOptionData } from 'discord.js';
+import { PermissionString, ApplicationCommandPermissionData, ApplicationCommandOptionData } from 'discord.js';
 import { Client } from '../src/utils/client';
+import { CommandOptions, Subcommand, SubcommandGroup } from './typings';
 
 class HandlerCommand {
     client: Client;
@@ -107,49 +108,3 @@ function getSubcommandOptions (subcommands: { [x: string]: Subcommand }) {
 
     return options;
 }
-
-declare interface CommandOptions {
-    name: string;
-    description: string;
-    category?: string;
-    options?: ApplicationCommandOptionData[];
-    defaultPermission?: boolean;
-    permissions?: ApplicationCommandPermissionData[]
-    development?: boolean;
-    devOnly?: boolean;
-    hideCommand?: boolean;
-    ownerOnly?: boolean;
-    guildOnly?: boolean;
-    perms?: PermissionString[];
-    clientPerms?: PermissionString[];
-    nsfw?: boolean;
-    groups?: { [x: string]: SubcommandGroup } | null;
-    subcommands?: { [x: string]: Subcommand } | null;
-}
-
-declare interface SubcommandGroup {
-    description: string;
-    subcommands: { [x: string]: Subcommand }
-}
-
-declare interface Subcommand {
-    description: string;
-    args?: Argument[];
-    execute? ({ client, interaction, group, subcommand }: { client: Client, interaction: CommandInteraction, group: string, subcommand: string }): any;
-}
-
-declare interface Argument {
-    type: 'STRING'|'INTEGER'|'BOOLEAN'|'USER'|'CHANNEL'|'ROLE'|'MENTIONABLE'|'NUMBER';
-    name: string;
-    description: string;
-    choices?: Choice[];
-    required?: boolean;
-    channelTypes?: ChannelTypes[];
-}
-
-declare interface Choice {
-    name: string;
-    value: string|number;
-}
-
-type ChannelTypes = | 'GUILD_TEXT' | 'DM' | 'GUILD_VOICE' | 'GROUP_DM' | 'GUILD_CATEGORY' | 'GUILD_NEWS' | 'GUILD_STORE' | 'GUILD_NEWS_THREAD' | 'GUILD_PUBLIC_THREAD' | 'GUILD_PRIVATE_THREAD' | 'GUILD_STAGE_VOICE';
